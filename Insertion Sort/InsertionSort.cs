f9cf27c9fa466e7a7aae4cc9546154bf33a7d53c
jei-W <gkdtkdv123@naver.com>
2019-02-22 02:38:26 +0900

    Insertion Sort의 구현 == *오름차순 정렬 구현 *내림차순 정렬 구현

---
+++ Insertion Sort/InsertionSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class InsertionSort
+    {
+        public static void AscendingSort(int[] arg)
+        {
+            for( int i = 1; i < arg.Length; i++ )
+            {
+                int tmp = arg[i];
+
+                for( int j = i - 1; j >= 0; j-- )
+                {
+                    if( tmp < arg[j] )
+                    {
+                        arg[j + 1] = arg[j];
+
+                        if( j == 0 )
+                        {
+                            arg[j] = tmp;
+                        }
+                    }
+                    else
+                    {
+                        arg[j + 1] = tmp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static void DescendingSort( int[] arg )
+        {
+            for( int i = 1; i < arg.Length; i++ )
+            {
+                int tmp = arg[i];
+
+                for( int j = i - 1; j >= 0; j-- )
+                {
+                    if( tmp > arg[j] )
+                    {
+                        arg[j + 1] = arg[j];
+
+                        if( j == 0 )
+                        {
+                            arg[j] = tmp;
+                        }
+                    }
+                    else
+                    {
+                        arg[j + 1] = tmp;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Insertion Sort/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            int[] arg = { 5, 9, 7, 24, 0, 8, 3, 7 };
+
+            Print();
+
+            InsertionSort.AscendingSort(arg);
+            Print();
+
+            InsertionSort.DescendingSort(arg);
+            Print();
+
+
+            void Print()
+            {
+                foreach( int i in arg )
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
